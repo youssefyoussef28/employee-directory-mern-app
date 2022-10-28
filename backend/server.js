5a9a1ca27b02589e3a2853cfddc8228d8991f0bd
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-28 14:58:59 +0300

    Creating user Routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.status(201).json({ message: "Welcome" });
+// });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
